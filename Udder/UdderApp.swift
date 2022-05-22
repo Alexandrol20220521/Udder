077cb4ea48aca9a7a96a41da3c04777f5b6230e3
Ryutaro <turuwatari@gmail.com>
2022-05-22 12:10:34 +0900

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by 中島竜太郎 on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
